179733783a6195f7ad290e93c0cf0f22f3915682
TheSeanicles <psean6@vt.edu>
2021-03-22 21:29:50 -0400

    Update Sleep.ino

--- Sensor_Node/Sleep.ino
+++ Sensor_Node/Sleep.ino
@@ -1,9 +1,8 @@
 void Sleep(){
+  attachInterrupt(digitalPinToInterrupt(2), Wakeup, FALLING);
   MCUCR = B01100000;
   SMCR = B00000101;
   __asm__ ("SLEEP");
-  attachInterrupt(digitalPinToInterrupt(2), Wakeup, FALLING);
-  delay(1000);
 }
 
 void Wakeup(){
